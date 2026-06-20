17b4f125a2736be3aac1a91e73103f3aced5ce36
achillesprojects07-bit <achilles.projects07@gmail.com>
2026-06-20 18:44:25 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.BECOMING_ROOM_FIREBASE_CONFIG = {
   apiKey: "AIzaSyC6KHF9c_lwy-sKZM5VSA-tmydKc42zmEE",
   authDomain: "becoming-room.firebaseapp.com",
   projectId: "becoming-room",
@@ -15,7 +7,3 @@ const firebaseConfig = {
   appId: "1:646188920622:web:002469d2d681824a7214bf",
   measurementId: "G-G8KNYMGFZ9"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
